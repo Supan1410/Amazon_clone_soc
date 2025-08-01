86696924b6174726a7d8bd115547ae643150dcb8
Supan Shah <supancshah2005@gmail.com>
2025-08-01 22:48:34 +0530

    Submit Soc

--- FInal/amazon/lib/main.dart
+++ FInal/amazon/lib/main.dart
@@ -8,8 +8,6 @@ import 'package:amazon/router.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
-
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
@@ -49,6 +47,15 @@ class _MyAppState extends State<MyApp> {
           iconTheme: IconThemeData(
             color: Colors.black,
           ),
+          backgroundColor: GlobalVariables.secondaryColor,
+          foregroundColor: Colors.white
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: GlobalVariables.secondaryColor, // Background color
+            foregroundColor: GlobalVariables.secondaryColor, // Text (and icon) color
+          ),
+        
         ),
         useMaterial3: true, // can remove this line
       ),
